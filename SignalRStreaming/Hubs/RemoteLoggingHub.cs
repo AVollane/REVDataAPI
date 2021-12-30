a01d2d3602698117e851c5b82a077c7a25286a98
AntWorkingV@outlook.com <AntWorkingV@outlook.com>
2021-12-30 14:27:09 +0300

    Added new project into solution
    - Added SignalRStreaming.csproj into solution
    - Added hubs folder in SignalRStreaming

---
+++ SignalRStreaming/Hubs/RemoteLoggingHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRStreaming.Hubs
+{
+    internal class RemoteLoggingHub : Hub
+    {
+    }
+}
